247b95d3914a79e7d8b14e0b1489997d7debde3a
Tom Dickson <tom@tomdickson.eu  name = Tom Dickson>
2018-04-04 21:56:42 +0200

    Fixed so that eBay properly fetches European results.

--- application/fetchers/ebay_fetchers.py
+++ application/fetchers/ebay_fetchers.py
@@ -6,24 +6,27 @@ def url_encode(string) :
 	return "%20".join(string.split(" "))
 
 def search_for_offer(api_key, country, product_name, price) :
+	if country is 'UK' :
+		country = 'GB'
 	try:
 		request_string = str("http://svcs.ebay.com/services/search/FindingService/v1?" + 
 			"OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&" + 
 			"SECURITY-APPNAME={0}&" + 
-			"GLOBAL-ID=EBAY-GB&" +
+			"GLOBAL-ID=EBAY-{1}&" +
 			"RESPONSE-DATA-FORMAT=XML&" +
 			"REST-PAYLOAD&" +
 				"affiliate.networkId=9&" +
 				"affiliate.trackingId=5338271690&" +
-				"keywords={1}&" + 
+				"keywords={2}&" + 
 				"itemFilter(0).name=Condition&itemFilter(0).value=1000&" +
 				"itemFilter(1).name=SellerBusinessType&itemFilter(1).value=Business&" +
 				"itemFilter(2).name=TopRatedSellerOnly&itemFilter(2).value=true&" +
 				"itemFilter(3).name=HideDuplicateItems&itemFilter(3).value=true&" +
-				"itemFilter(4).name=MinPrice&itemFilter(4).value={2}&" +
+				"itemFilter(4).name=MinPrice&itemFilter(4).value={3}&" +
 				"outputSelector=SellerInfo"
 		).format(
 			api_key['APPNAME'],
+			country,
 			url_encode(product_name),
 			str(float(price) * 0.50)
 		)
--- application/main.py
+++ application/main.py
@@ -10,13 +10,16 @@ from jobs import sync_kelkoo_offers
 
 # sync_amazon_products('UK')
 # sync_amazon_offers('UK')
-
 # sync_amazon_products('DE')
 # sync_amazon_offers('DE')
-
-sync_amazon_products('ES')
-sync_amazon_offers('ES')
-
+# sync_amazon_products('IT')
+# sync_amazon_offers('IT')
+# sync_amazon_products('ES')
+# sync_amazon_offers('ES')
 
 # sync_ebay_offers('UK')
+# sync_ebay_offers('DE')
+sync_ebay_offers('FR')
+
+
 # sync_kelkoo_offers('UK')
